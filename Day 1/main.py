e310ae8a64ea5fef365d0c11280420409f8edd13
andrew-lange13 <andrew.lange13@gmail.com>
2022-09-27 17:36:55 -0700

    day 1 of 100 complete

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,1 +1,7 @@
-print("still testing")
+print("\nWelcome to the Band Name Generator")
+
+city = input("\nWhat city did you grow up in?\n")
+
+pet_name = input("What is you pets name?\n")
+
+print("Your band name is " + city + " " + pet_name)
