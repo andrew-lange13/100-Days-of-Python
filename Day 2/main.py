4c722f69df708f58a1e4c50f08c113d423d5c57d
ANDREW LANGE <andrew.lange13@gmail.com>
2022-10-17 16:23:05 -0700

    finished day 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -4,6 +4,6 @@ bill = float(input("How much was the bill?  "))
 people = int(input("How many people is it being split between?  "))
 tipPercentage = int(input("What percentage do you want to tip?  "))
 
-amtToPay = round((bill / people) * ((tipPercentage/100) + 1), 2)
+amtToPay = "{:.2f}".format(round((bill / people) * ((tipPercentage/100) + 1), 2))
 
 print(f"Each person should pay ${amtToPay}")
